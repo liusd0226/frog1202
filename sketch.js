2fb8aedf455429c3931b8d7d20afe2c0a98a528e
liusd0226 <liusd0226@gmail.com>
2020-12-02 15:03:36 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -29,6 +29,8 @@ function setup() {
   // Start classifying
   // The sound model will continuously listen to the microphone
   classifier.classify(gotResult);
+  ThunkableWebviewerExtension.postMessage("ready");
+
 }
 
 function draw() {
@@ -50,4 +52,5 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+  ThunkableWebviewerExtension.postMessage(label);
 }
